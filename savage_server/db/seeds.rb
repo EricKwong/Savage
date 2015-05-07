4f0e16e998e8bc64ddbe24f189884a3c17e0d60f
Eric Kwong <erickwong618@yahoo.com>
2015-05-07 14:47:26 -0400

    seeded db

--- savage_server/db/seeds.rb
+++ savage_server/db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.destroy_all
+Monster.destroy_all
+
+
+4.times do
+  Player.create!({
+    username: 'PoopySamurai',
+    password_digest: 'poop',
+    first_name: 'Eric',
+    last_name: 'Kwong',
+    level: 10,
+    hp: 300,
+    attack: 30,
+    defense: 30,
+    exp: 1000,
+    gold: 15000,
+    steps: 1000
+  })
+end
+
+4.times do
+  Monster.create!({
+    name: 'Booboo',
+    attack: 15,
+    defense: 17,
+    hp: 150,
+    rarity: 4,
+    exp: 100,
+    bounty: 180
+  })
+end
