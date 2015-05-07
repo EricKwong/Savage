7b2f6abca79d94afeeb89e59e7572e7c3ce52cea
Eric Kwong <erickwong618@yahoo.com>
2015-05-07 14:45:20 -0400

    seeded db, created login and main menu view

---
+++ savage_app/MainMenuView.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  Image
+} = React;
+
+var MainMenuView = React.createClass({
+  render: function() {
+    console.log('rendered')
+    return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        Hello
+      </Text>
+    </View>
+    )
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  name: {
+    fontSize: 18,
+    color: 'black'
+  }
+});
+
+module.exports = MainMenuView;
--- savage_app/index.ios.js
+++ savage_app/index.ios.js
@@ -5,49 +5,32 @@
 'use strict';
 
 var React = require('react-native');
+var LogInView = require('./LogInView');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
+  NavigatorIOS
 } = React;
 
 var savage_app = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS style={styles.container}
+        initialRoute={{
+          title: 'Log In',
+          component: LogInView
+        }}/>
     );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('savage_app', () => savage_app);
